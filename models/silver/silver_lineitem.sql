1373ed0cacdd58a244536e33b4184f4c06513eef
Muhammad Alam <shaonetv@gmail.com>
2026-06-21 02:06:17 +0000

    fixed some field name to match bronze model

--- models/silver/silver_lineitem.sql
+++ models/silver/silver_lineitem.sql
@@ -1,51 +1,27 @@
-with lineitem as (
-      select * from {{ ref('bronze_lineitem') }}
-  ),
-  orders as (
-      select * from {{ ref('bronze_orders') }}
-  ),
-  part as (
-      select * from {{ ref('bronze_part') }}
-  ),
-  supplier as (
-      select * from {{ ref('bronze_supplier') }}
-  )
+with
+    cte_lineitem as (select * from {{ ref("bronze_lineitem") }}),
+    cte_orders as (select * from {{ ref("bronze_orders") }}),
+    cte_part as (select * from {{ ref("bronze_part") }}),
+    cte_supplier as (select * from {{ ref("bronze_supplier") }})
 
-  select
-      l.order_key,
-      l.line_number,
-      l.quantity,
-      l.extended_price,
-      l.discount,
-      l.tax,
-      l.return_flag,
-      l.ship_date,
-      l.ship_mode,
-      l.extended_price * (1 - l.discount) as net_price,
-      o.order_date,
-      o.order_status,
-      p.name            as part_name,
-      p.type            as part_type,
-  )
-
-  select
-      l.order_key,
-      l.line_number,
-      l.quantity,
-      l.extended_price,
-      l.discount,
-      l.tax,
-      l.return_flag,
-      l.ship_date,
-      l.ship_mode,
-      l.extended_price * (1 - l.discount) as net_price,
-      o.order_date,
-      o.order_status,
-      p.name            as part_name,
-      p.type            as part_type,
-      s.name            as supplier_name,
-      s.nation_key      as supplier_nation_key
-  from lineitem l
-  left join orders   o on l.order_key  = o.order_key
-  left join part     p on l.part_key   = p.part_key
-  left join supplier s on l.supplier_key = s.supplier_key
+select
+    l.order_key,
+    l.line_number,
+    l.quantity,
+    l.extended_price,
+    l.discount,
+    l.tax,
+    l.return_flag,
+    l.ship_date,
+    l.ship_mode,
+    l.extended_price * (1 - l.discount) as net_price,
+    o.order_date,
+    o.order_status,
+    p.part_name,
+    p.type as part_type,
+    s.supplier_name,
+    s.nation_key as supplier_nation_key
+from cte_lineitem l
+left join cte_orders o on l.order_key = o.order_key
+left join cte_part p on l.part_key = p.part_key
+left join cte_supplier s on l.supplier_key = s.supplier_key
